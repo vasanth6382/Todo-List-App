ef3baf26ce6f03c2999dad66c70e3000de3005c7
vasanth6382 <114655928+vasanth6382@users.noreply.github.com>
2023-10-31 21:46:11 +0530

    Add files via upload

---
+++ todoscript.js
@@ -0,0 +1,32 @@
+function addTodo() {
+    const todoInput = document.getElementById("todo-input");
+    const todoList = document.getElementById("todo-list");
+
+    const todoText = todoInput.value.trim();
+
+    if (todoText !== "") {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${todoText}</span>
+            <button class="button" onclick="removeTodo(this)">Delete</button>
+        `;
+
+        li.addEventListener("click", toggleComplete);
+
+        todoList.appendChild(li);
+        todoInput.value = "";
+    }
+}
+
+function removeTodo(button) {
+    const li = button.parentElement;
+    li.remove();
+}
+
+function toggleComplete() {
+    this.classList.toggle("completed");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("todo-input").focus();
+});
